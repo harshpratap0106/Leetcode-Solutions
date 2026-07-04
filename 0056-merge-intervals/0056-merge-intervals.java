a4b152b814ca2aee32c3be0c0c23275ab46086cb
Harsh Pratap Singh Parihar <harshpratap.2723@gmail.com>
2026-07-04 18:04:39 +0530

    Time: 8 ms (91.46%), Space: 49.1 MB (59.46%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -0,0 +1,25 @@
+class Solution {
+
+    public int[][] merge(int[][] intervals) {
+
+        // Sort intervals based on starting time
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+        List<int[]> result = new ArrayList<>();
+
+        for (int[] interval : intervals) {
+
+            // If result is empty OR no overlap
+            if (result.isEmpty() || result.get(result.size() - 1)[1] < interval[0]) {
+                result.add(interval);
+            } 
+            else {
+                // Merge intervals
+                result.get(result.size() - 1)[1] =
+                    Math.max(result.get(result.size() - 1)[1], interval[1]);
+            }
+        }
+
+        return result.toArray(new int[result.size()][]);
+    }
+}
